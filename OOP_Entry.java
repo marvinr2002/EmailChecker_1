ad7d208b362b14f3edf0f42736582b23279dde2e
Marvin Ruiz <116685708+marvinr2002@users.noreply.github.com>
2025-09-18 14:35:09 -0400

    Minor Changes -- Attributes
    Name
    Age
    isOverAge?
    OOP Customer Class creation

---
+++ OOP_Entry.java
@@ -0,0 +1,23 @@
+public class OOP_Entry{
+public static void main(String[] args) {
+    Customer myClient = new Customer();
+
+}
+}
+
+class Strings{
+    /*
+     * L1 -Terminal App 
+     * L2 -CRUD con interfaz
+     * L3 - Database
+     * L4 - API
+     * L5 - Authentication
+     * L6 - Deploy
+     * L7 - SMS - Notifications
+     */
+}
+class Customer{ 
+private String name;
+private int Age;
+private boolean overAge; 
+}
